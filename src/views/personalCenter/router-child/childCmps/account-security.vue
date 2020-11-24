ae8a2d403ff64993d9d6b864695fbe9ca0f472d4
肖高杰 <8106645+xiao-gaojie@user.noreply.gitee.com>
2020-11-24 22:50:54 +0800

    完成个人中心重置密码

--- src/views/personalCenter/router-child/childCmps/account-security.vue
+++ src/views/personalCenter/router-child/childCmps/account-security.vue
@@ -9,14 +9,16 @@
       <div class="verify-code">
         <!-- 输入验证码 -->
         <input type="text" v-model="code" placeholder="输入验证码" />
-        <button class="btn" :disabled="disabledBtn" @click="send_code">{{textCode}}</button>
+        <button class="btn" :disabled="disabledBtn" @click="send_code">
+          {{ textCode }}
+        </button>
       </div>
       <!-- 下方多选框 -->
       <div class="radio-box">
         <div class="readio" @click="checkoutBtn(0)">
           <!-- incon -->
           <img
-            :src="currentActive==0?selected:unselect"
+            :src="currentActive == 0 ? selected : unselect"
             class="incon"
             alt=""
           />
@@ -26,7 +28,7 @@
         <div class="readio" @click="checkoutBtn(1)">
           <!-- incon -->
           <img
-            :src="currentActive==1?selected:unselect"
+            :src="currentActive == 1 ? selected : unselect"
             class="incon"
             alt=""
           />
@@ -35,14 +37,15 @@
       </div>
     </div>
     <!-- 重置密码 -->
-    <div v-if="currentActive==0" class="reset-paswd">
+    <div v-if="currentActive == 0" class="reset-paswd">
       <div class="title">重置密码</div>
       <input
         type="text"
+        v-model="userUpwd"
         placeholder="输入新密码（6-16位任意字符，区分大小写）"
       />
       <div style="margin-top: 10px">
-        <input type="text" placeholder="再次输入密码" />
+        <input type="text" v-model="redoUserUpwd" placeholder="再次输入密码" />
       </div>
     </div>
     <!-- 更换手机号 -->
@@ -58,50 +61,63 @@
     </div>
     <!-- footer提交按钮 -->
     <div class="footer">
-      <div class="footer-btn">提交</div>
+      <div class="footer-btn" @click="submit">提交</div>
     </div>
   </div>
 </template>
 <script>
+import { validatePhoneNumber } from "@/utils/regular";
+import {
+  passwordLogin,
+  captchaLogin,
+  queryCaptcha,
+  checkCaptcha,
+  test,
+} from "@/network/login";
+import { optResetPassword } from "@/network/personalCenter";
 export default {
   data() {
     return {
-        // 默认选中的单选框
-        currentActive:0,
-        // 选中icon
-        selected:require("@/assets/img/personalCenter/icon_radiobutton_selected.png"),
-        // 未选中icon
-        unselect:require("@/assets/img/personalCenter/icon_radiobutton.png"),
-        //发送验证码
-        textCode: "获取验证码",
-        disabledBtn:false,  //是否禁用按钮
-        phone:"",  //用户手机号
-        code:""    //输入验证码
+      // 默认选中的单选框
+      currentActive: 0,
+      // 选中icon
+      selected: require("@/assets/img/personalCenter/icon_radiobutton_selected.png"),
+      // 未选中icon
+      unselect: require("@/assets/img/personalCenter/icon_radiobutton.png"),
+      //发送验证码
+      textCode: "获取验证码",
+      disabledBtn: false, //是否禁用按钮
+      phone: 15828353333, //用户手机号
+      code: 1234, //用户输入验证码
+      serverCode: "", //服务器返回来的验证码
+      userUpwd: "", //用户重置的密码
+      redoUserUpwd: "", //用户重复输入密码
     };
   },
-  methods:{
-      checkoutBtn(i){
-          this.currentActive=i;
-          console.log("this.currentActive",this.currentActive)
-      },
-      // 发送验证码
-      send_code() {
-      let phone = this.inputList[1].value;
+  methods: {
+    checkoutBtn(i) {
+      this.currentActive = i;
+      console.log("this.currentActive", this.currentActive);
+    },
+    // 发送验证码
+    send_code() {
+      let phone = this.phone;
       if (!phone) {
-        alert("手机号码不能为空");
+        this.$myAlert("手机号码不能为空");
         return;
       }
       if (!validatePhoneNumber(phone)) {
-        alert("请输入正确的手机格式");
+        this.$myAlert("请输入正确的手机格式");
         return;
       }
       this.startTime();
     },
     // 启动定时器
-     startTime() {
+    startTime() {
       // 调用函数
       let i = 10;
       let timer = null;
+      this.get_queryCaptcha(this.phone, 2);
       timer = setInterval(() => {
         i--;
         if (i >= 0) {
@@ -114,8 +130,56 @@ export default {
           this.disabledBtn = false;
         }
       }, 1000);
-    }
-  }
+    },
+    // 获取手机验证码
+    async get_queryCaptcha(phone, type) {
+      let data = { phone: phone, type: type };
+      let res = await queryCaptcha(data);
+      console.log("消息获取成功", res);
+      this.serverCode = res;
+    },
+    // 传给后端，后端校验验证码
+    async get_checkCaptcha() {
+      let data = { phone: this.phone, code: this.code };
+      let res = await checkCaptcha(data);
+    },
+    // 提交表单给后台
+    async submit() {
+      if (!this.userPhone) {
+        alert("手机号码不能为空");
+        return;
+      }
+      if (!this.code) {
+        alert("验证码不能为空");
+        return;
+      }
+      if (!this.userUpwd) {
+        alert("新密码不能为空");
+        return;
+      }
+      if (!this.redoUserUpwd) {
+        alert("请再次输入密码");
+        return;
+      }
+      if (validatePhoneNumber(this.upwdForm.userPhone)) {
+        alert("请输入正确的手机格式");
+        return;
+      }
+      if (this.upwdForm.userUpwd !== this.upwdForm.redoUserUpwd) {
+        alert("两次输入的密码不一致");
+        return;
+      }
+      let result = await get_checkCaptcha();
+      // 后端校验验证码，如果正确，调用修改密码接口，否则return 重新输入验证码
+      let data = {
+        code: this.code,
+        newPassword: this.userUpwd,
+        phone: this.phone,
+      };
+      let res = await optResetPassword(data);
+      console.log("获取重置密码的结果", res);
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
--- src/views/personalCenter/router-child/childCmps/personal-data.vue
+++ src/views/personalCenter/router-child/childCmps/personal-data.vue
@@ -1,60 +1,65 @@
 <template>
-    <div class="box">
-        <!-- 头部个人资料 -->
-        <div class="title">个人资料</div>
-        <!-- 中间内容 -->
-        <div class="main">
-            <!-- 基本信息 -->
-            <div class="nav-bar">
-                <span
-                @click="go_url(item.path),checkoutTitle(index)"
-                :class="{active:currentIndex==index}" 
-                v-for="(item,index) in navbarList"
-                :key="index"
-                >{{item.title}}</span>
-                <!-- <span>账号安全</span> -->
-            </div>
-            <!-- 判断是否是基本信息还是账号安全 -->
-            <div>
-                <router-view></router-view>
-            </div>
-        </div>
+  <div class="box">
+    <!-- 头部个人资料 -->
+    <div class="title">个人资料</div>
+    <!-- 中间内容 -->
+    <div class="main">
+      <!-- 基本信息 -->
+      <div class="nav-bar">
+        <span
+          @click="go_url(item.path), checkoutTitle(index)"
+          :class="{ active: isRouter(item.path) }"
+          v-for="(item, index) in navbarList"
+          :key="index"
+          >{{ item.title }}</span
+        >
+        <!-- <span>账号安全</span> -->
+      </div>
+      <!-- 判断是否是基本信息还是账号安全 -->
+      <div>
+        <router-view></router-view>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-        return {
-                navbarList:[
-                {
-                    path:"personal-info",
-                    title:"基本信息"
-                },{
-                    path:"account-security",
-                    title:"账号安全"
-                }
-            ],
-            // 默认选中基本信息
-            currentIndex:0
-        }
-    },
-    methods:{
-        go_url(url){
-            this.$router.push({
-                path:"/page/personalCenter/personal/data/"+url
-            })
+  data() {
+    return {
+      navbarList: [
+        {
+          path: "personal-info",
+          title: "基本信息",
         },
-        checkoutTitle(index){
-            this.currentIndex=index;
-        }
-    }
-}
+        {
+          path: "account-security",
+          title: "账号安全",
+        },
+      ],
+      // 默认选中基本信息
+      currentIndex: 0,
+    };
+  },
+  methods: {
+    isRouter(url) {
+      return url == this.$route.name;
+    },
+    go_url(url) {
+      this.$router.push({
+        path: "/page/personalCenter/personal/data/" + url,
+      });
+    },
+    checkoutTitle(index) {
+      this.currentIndex = index;
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
-.box{
-    // padding-left: 44px;
+.box {
+  // padding-left: 44px;
 }
-    .title {
+.title {
   font-size: 24px;
   font-family: Source Han Sans CN;
   font-weight: 500;
@@ -62,28 +67,27 @@ export default {
   margin: 20px 0;
 }
 // 选中
-.active{
-    
-font-size: 20px;
-font-family: Source Han Sans CN;
-font-weight: bold;
-color: #333333;
+.active {
+  font-size: 20px;
+  font-family: Source Han Sans CN;
+  font-weight: bold;
+  color: #333333;
 }
-.main{
-    width: 1014px;
-// height: 1080px;
+.main {
+  width: 1014px;
+  // height: 1080px;
   padding: 18px 23px;
-background: #F8F9F9;
-.nav-bar{
+  background: #f8f9f9;
+  .nav-bar {
     width: 100%;
     font-size: 18px;
     font-family: Source Han Sans CN;
     font-weight: 500;
-    color: #7B7B7B;
+    color: #7b7b7b;
     line-height: 54px;
-    & >:first-child{
-        margin-right: 25px;
+    & > :first-child {
+      margin-right: 25px;
     }
-}
+  }
 }
 </style>
